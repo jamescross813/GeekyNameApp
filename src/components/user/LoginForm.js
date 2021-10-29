f970331fc7a07ab67e32383390eab8578a074d03
jamescross813 <crossjames813@gmail.com>
2021-10-28 21:37:07 -0500

    login form working and updating state

--- src/components/user/LoginForm.js
+++ src/components/user/LoginForm.js
@@ -19,13 +19,13 @@ class LoginForm extends Component {
 
     handleChange = (event) =>{
         this.setState({
-          text: event.target.value
+          [event.target.name]: event.target.value
         })
       }
 
     handleSubmit = (event) =>{
         event.preventDefault()
-        this.props.addRest(this.state)
+        console.log(this.state)
         this.setState({
           text: ""
         })
@@ -35,9 +35,9 @@ class LoginForm extends Component {
         return (
           <form onSubmit={this.handleSubmit}>
               <label>Username</label><br/>
-              <input type="text" onChange={this.handleChange} /><br/>
+              <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br/>
               <label>Password</label><br/>
-              <input type="password" onChange={this.handleChange} /><br/>
+              <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br/>
               <input type="submit" />
           </form>
         );
