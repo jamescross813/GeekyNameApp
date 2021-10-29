59e04bb581fd9a125d72289c417beb3737d65e2e
jamescross813 <crossjames813@gmail.com>
2021-10-28 21:15:17 -0500

    login form render added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LoginForm from "./components/user/LoginForm"
 
 function App() {
   return (
     <div className="App">
       {/* login form conponent*/}
+      <LoginForm />
       {/* homepage presentation component */}
     </div>
   );
