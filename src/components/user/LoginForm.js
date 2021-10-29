b4cc6d770001f2853595e4432a88a17be265b78f
jamescross813 <crossjames813@gmail.com>
2021-10-28 21:28:37 -0500

    handleSubmit added

--- src/components/user/LoginForm.js
+++ src/components/user/LoginForm.js
@@ -17,15 +17,23 @@ class LoginForm extends Component {
         password: ""
     }
 
-    // handleChange = (event) =>{
-    //     this.setState({
-    //       text: event.target.value
-    //     })
-    //   }
+    handleChange = (event) =>{
+        this.setState({
+          text: event.target.value
+        })
+      }
+
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        this.props.addRest(this.state)
+        this.setState({
+          text: ""
+        })
+      }
 
     render() {  
         return (
-          <form>
+          <form onSubmit={this.handleSubmit}>
               <label>Username</label><br/>
               <input type="text" onChange={this.handleChange} /><br/>
               <label>Password</label><br/>
