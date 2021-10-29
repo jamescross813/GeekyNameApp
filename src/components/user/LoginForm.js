58cb19f60667ad60bb9a13ce86c46ea8066873a7
jamescross813 <crossjames813@gmail.com>
2021-10-28 21:12:59 -0500

    basic form written

--- src/components/user/LoginForm.js
+++ src/components/user/LoginForm.js
@@ -12,5 +12,13 @@ import React, { Component } from 'react';
 
 class LoginForm extends Component {
 
-   
+    render() {  
+        return (
+          <form>
+              <input type="text" onChange={this.handleChange} />
+              <input type="password" onChange={this.handleChange} />
+              <input type="submit" />
+          </form>
+        );
+      }
 };
