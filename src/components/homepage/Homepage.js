c70044278815b9cb2a166cf59b7f7788a1f17d61
jamescross813 <crossjames813@gmail.com>
2021-10-29 12:48:07 -0500

    linking to pages attempt

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,14 +1,18 @@
 import { Component } from "react";
 import { Link } from "react-router-dom"
+import LoginForm from "../user/LoginForm"
 
 class Homepage extends Component{
+    renderPage =()=>{
+        return <LoginForm handleLogin={this.login}/>
+    }
 
     render(){
         return(
         <div>
             <button>Login</button>
             <button>Signup</button>
-            <Link to exact path = "/signup" render={()=> <LoginForm handleLogin={this.login} />} />
+            <Link to= "/signup" exact onCLick={this.renderPage} >Login</Link>
         </div>
         )
     }
