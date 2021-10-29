2ce7a95c1623fe5c4d7bf8ad7064a28c7ced4da0
jamescross813 <crossjames813@gmail.com>
2021-10-29 12:56:54 -0500

    header added

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -7,9 +7,10 @@ class Homepage extends Component{
     render(){
         return(
         <div>
-            
+            <h1>Geeky Name App</h1>
             <Link to= "/signup" exact >Sign up</Link>
             <Link to= "/login" exact >Login</Link>
+
         </div>
         )
     }
