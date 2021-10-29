ad8431b795572a46f27f2535a8d7db90218950e0
jamescross813 <crossjames813@gmail.com>
2021-10-28 22:46:45 -0500

    basic render function included

--- src/containers/user/UserHomepageContainer.js
+++ src/containers/user/UserHomepageContainer.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class UserHomepageContianer extends Component{
 
+    render(){
+        return(
+        <div>
+            Here now
+        </div>
+        )
+    }
 }
 
 export default UserHomepageContianer
