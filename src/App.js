c2e7f5b56af7489edb3b3a14e3e8dc2cfd11fc1c
jamescross813 <crossjames813@gmail.com>
2021-10-28 22:44:57 -0500

    updated rendering for User Homepage Container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
 import './App.css';
 import LoginForm from "./components/user/LoginForm"
+import UserHomepageContainer from "./containers/user/UserHomepageContainer"
 
 class App extends Component {
 
@@ -16,7 +17,7 @@ class App extends Component {
           {/* login form conponent*/}
           <Route to exact path = "/" render={()=> <LoginForm handleLogin={this.login} />} />
           {/* homepage presentation component */}
-          <Route to exact path = "/user/1" render={()=> <HomepageContainer/>}/>
+          <Route to exact path = "/user/1" render={()=> <UserHomepageContainer/>}/>
         </div>
       </Router>
     )
