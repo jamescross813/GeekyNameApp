5d0a5096d26d34e76c28c05890268f2f920f9586
jamescross813 <crossjames813@gmail.com>
2021-10-29 12:54:24 -0500

    removed unnecessary code

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom"
 import LoginForm from "../user/LoginForm"
 
 class Homepage extends Component{
-    
+
     render(){
         return(
         <div>
-            <button>Login</button>
-            <button>Signup</button>
+            
             <Link to= "/signup" exact onCLick={this.renderPage} >Login</Link>
         </div>
         )
