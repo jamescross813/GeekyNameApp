a71a5fcb9353010f3eea2d39121dee4ca489b209
jamescross813 <crossjames813@gmail.com>
2021-10-29 20:42:45 -0500

    included nav links

--- src/components/navbar/HomepageNavBar.js
+++ src/components/navbar/HomepageNavBar.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const HomepageNavBar = () => {
-  
+    return(
+        <div className = "HomepageNavBar">
+    <NavLink to= "/signup" exact >Sign up</NavLink>
+    <NavLink to= "/login" exact >Login</NavLink>
+    </div>
+    )
 };
 
 export default HomepageNavBar;
