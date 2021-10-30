88f2210c5ce123bd597cc3e48f93ccefa7f3b87a
jamescross813 <crossjames813@gmail.com>
2021-10-29 21:59:44 -0500

    basic function

---
+++ src/containers/group/GroupsPageContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import GroupsPage from "../../components/group/GroupsPage"
+
+class GroupsPageContainer extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default GroupsPageContainer
