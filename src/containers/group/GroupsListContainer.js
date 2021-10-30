38fc40accb601315666dc6017ffc056bfd4a3752
jamescross813 <crossjames813@gmail.com>
2021-10-30 14:52:48 -0500

    fetch method written

--- src/containers/group/GroupsListContainer.js
+++ src/containers/group/GroupsListContainer.js
@@ -6,7 +6,17 @@ class GroupsListContainer extends Component{
     state={
         groups: []
     }
-    
+
+    componentDidMount(){
+        fetch("http://localhost:3000/groups")
+        .then(resp=>resp.json())
+        .then(data=>{
+            this.setState({
+                groups: data
+            })
+        })
+    }
+
     render(){
         return(
         <div>
