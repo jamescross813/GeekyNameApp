bc6b94755870718936829f244e402f365f4f6885
jamescross813 <crossjames813@gmail.com>
2021-10-29 20:58:41 -0500

    added file paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
 import './App.css';
+import Homepage from "./components/homepage/Homepage";
 import LoginForm from "./components/user/LoginForm"
 import UserHomepageContainer from "./containers/user/UserHomepageContainer"
-import Homepage from "./components/homepage/Homepage";
-import NavBar from "./components/navbar/NavBar";
+import GroupsPageContainer from "./containers/group/GroupsPageContainer"
+import FriendsPageContainer from "./containers/friend/FriendsPageContainer"
+import EventsPageContainer from "./containers/event/EventsPageContainer"
 
 class App extends Component {
 
