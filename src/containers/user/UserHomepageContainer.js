ee35d2c9de247da3c6f8199743eccebfca5840c9
jamescross813 <crossjames813@gmail.com>
2021-10-29 19:56:12 -0500

    added import

--- src/containers/user/UserHomepageContainer.js
+++ src/containers/user/UserHomepageContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import UserHomepage from "../components/UserHomepage"
 
 class UserHomepageContainer extends Component{
 
     render(){
         return(
         <div>
-            You are here user
+            <UserHomepage />
         </div>
         )
     }
