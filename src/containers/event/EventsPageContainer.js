1f80053c029aa71b60a068a83a8640464953d696
jamescross813 <crossjames813@gmail.com>
2021-10-29 21:48:47 -0500

    corrected path name

--- src/containers/event/EventsPageContainer.js
+++ src/containers/event/EventsPageContainer.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import EventsPage from "../components/event/EventsPage"
+import EventsPage from "../../components/event/EventsPage"
 
 class EventsPageContainer extends Component{
     render(){
