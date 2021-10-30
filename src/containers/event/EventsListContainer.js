fcb9eced2382d0bfc7f735884dce6e8c25671b0f
jamescross813 <crossjames813@gmail.com>
2021-10-30 11:19:28 -0500

    initial state set

--- src/containers/event/EventsListContainer.js
+++ src/containers/event/EventsListContainer.js
@@ -2,6 +2,14 @@ import { Component } from "react";
 import EventsList from "../../components/event/EventsList";
 
 class EventsListContainer extends Component{
+    state={
+        event_name: "",
+        description:"",
+        est_time: "",
+        game_type: ""
+    }
+
+   
     render(){
         return(
         <div>
