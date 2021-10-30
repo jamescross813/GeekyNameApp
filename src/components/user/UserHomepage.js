e066a96a769a5865ec0e69f382a2e1cafca9e382
jamescross813 <crossjames813@gmail.com>
2021-10-29 20:35:03 -0500

    navbar render

--- src/components/user/UserHomepage.js
+++ src/components/user/UserHomepage.js
@@ -1,11 +1,13 @@
 // include navbar
 // render friends, groups, events, avialability
 import { Component } from "react";
+import NavBar from "../navbar/NavBar";
 
 class UserHomepage extends Component{
     render(){
         return(
         <div>
+            <NavBar />
                 This Page
         </div>
         )
