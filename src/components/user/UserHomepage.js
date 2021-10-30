87ffc0be4c08bafc1c26acdbe26029b21e9913bd
jamescross813 <crossjames813@gmail.com>
2021-10-29 19:57:32 -0500

    basic class structure

--- src/components/user/UserHomepage.js
+++ src/components/user/UserHomepage.js
@@ -1,2 +1,13 @@
 // include navbar
 // render friends, groups, events, avialability
+import { Component } from "react";
+
+class UserHomepage extends Component{
+    render(){
+        <div>
+
+        </div>
+    }
+}
+
+export default UserHomepage
