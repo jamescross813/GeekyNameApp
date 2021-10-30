422ee5cb6d1e806817bd15acd0eb53d1c702aa15
jamescross813 <crossjames813@gmail.com>
2021-10-29 22:08:21 -0500

    basic structure

---
+++ src/containers/friend/FriendContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import FriendPage from "../../components/friend/FriendPage"
+
+class FriendContainer extends Component{
+    render(){
+        return(
+            <div>
+                <FriendPage />
+            </div>
+        )
+    }
+}
+
+export default FriendContainer
