314782022f33f2d10cc62bdaf4d8927610f71ebb
jamescross813 <crossjames813@gmail.com>
2021-10-29 22:30:00 -0500

    render added

--- src/containers/event/EventsListContainer.js
+++ src/containers/event/EventsListContainer.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
+import EventsList from "../../components/event/EventsList";
 
 class EventsListContainer extends Component{
     render(){
         <div>
-
+            <EventsList />
         </div>
     }
 }
