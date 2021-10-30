38da1d44d208046ed10f6669a1aab034b4292dd4
jamescross813 <crossjames813@gmail.com>
2021-10-29 20:51:13 -0500

    moved NavBar render

--- src/App.js
+++ src/App.js
@@ -15,11 +15,12 @@ class App extends Component {
   render() {
     return(
       <Router>
-        <NavBar />
+        
         <div className="App">
           <Route to exact path = "/" render={()=><Homepage />}/>
           <Route to exact path="/signup" render={()=><LoginForm handleLogin={this.login} />}/>
           <Route to exact path="/login" render={()=><LoginForm handleLogin={this.login} />}/>
+          <NavBar />
           <Route to path="/user/1" render={()=><UserHomepageContainer />} />
           <Route to path="/groups"/>
           <Route to path="/friends" />
