2c9e47abc648ab48c3c2720c85d7ad8b02066422
jamescross813 <crossjames813@gmail.com>
2021-10-29 20:26:29 -0500

    nav bar basic structure

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,1 +1,14 @@
 // links to events, friends, log out, groups
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      
+    </div>
+  );
+};
+
+export default NavBar;
