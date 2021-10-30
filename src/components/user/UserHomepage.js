70e7bc59cdc1d1b30d4a9031928898e5bed09653
jamescross813 <crossjames813@gmail.com>
2021-10-29 23:10:35 -0500

    Events Page render

--- src/components/user/UserHomepage.js
+++ src/components/user/UserHomepage.js
@@ -4,13 +4,16 @@ import { Component } from "react";
 import NavBar from "../navbar/NavBar";
 import EventsListContainer from "../../containers/event/EventsListContainer"
 import FriendsListContainer from "../../containers/friend/FriendsListContainer"
-import GroupsListContainer from "../../containers/group/GroupsListContainer";
+import GroupsListContainer from "../../containers/group/GroupsListContainer"
+import EventsPageContainer from "../../containers/event/EventsPageContainer";
 
 class UserHomepage extends Component{
     render(){
         return(
         <div>
             <NavBar />
+            <EventsPageContainer />
+            
             <EventsListContainer />
             <FriendsListContainer />
             <GroupsListContainer />
