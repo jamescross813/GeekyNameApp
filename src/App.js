713ef8c10b5b24b0c1ad57bea83144f05e3fb871
jamescross813 <crossjames813@gmail.com>
2021-10-29 19:43:28 -0500

    additional routes added

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ class App extends Component {
           <Route to exact path="/signup" render={()=><LoginForm handleLogin={this.login} />}/>
           <Route to exact path="/login" render={()=><LoginForm handleLogin={this.login} />}/>
           <Route to path="/user/1" render={()=><UserHomepageContainer />} />
+          <Route to path="/groups"/>
+          <Route to path="/friends" />
+          <Route to path="/events" />
+          <Route to path="/availability" />
         </div>
         
       </Router>
