1fe193a45e9a9d376c918f0bbd039324650d60e0
jamescross813 <crossjames813@gmail.com>
2021-10-29 19:40:53 -0500

    link added

--- src/components/user/LoginForm.js
+++ src/components/user/LoginForm.js
@@ -40,7 +40,7 @@ class LoginForm extends Component {
               <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br/>
               <label>Password</label><br/>
               <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br/>
-              <Link>
+              <Link to="/user/1">
               <input type="submit" />
               </Link>
           </form>
