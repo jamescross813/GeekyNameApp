713e7c692f9c8de209f74a6c5d7794e141e9fafd
jamescross813 <crossjames813@gmail.com>
2021-10-29 21:46:56 -0500

    events page container basic

---
+++ src/containers/event/EventsPageContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import EventsPage from "../components/event/EventsPage"
+
+class EventsPageContainer extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default EventsPageContainer
