9eba15a7f8ea1be5c717de591a3c43473c9e2b60
jamescross813 <crossjames813@gmail.com>
2021-10-29 22:31:30 -0500

    render added

--- src/containers/group/GroupsListContainer.js
+++ src/containers/group/GroupsListContainer.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
+import GroupsList from  "../../components/GroupsList"
 
 class GroupsListContainer extends Component{
     render(){
         <div>
-
+            <GroupsList />
         </div>
     }
 }
