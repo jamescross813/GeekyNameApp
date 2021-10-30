4c6834e8fddbc1c916aeb7baf82d50c2c515962d
jamescross813 <crossjames813@gmail.com>
2021-10-29 21:55:21 -0500

    basic function

---
+++ src/containers/friend/FriendsPageContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import FriendsPage from "../../components/friend/FriendsPage"
+
+class FriendsPageContainer extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FriendsPageContainer
