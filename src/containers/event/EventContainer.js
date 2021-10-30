93be3b9ffa0a825560a0fb2d86fbda614963deb7
jamescross813 <crossjames813@gmail.com>
2021-10-29 22:03:35 -0500

    basic class added

---
+++ src/containers/event/EventContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import EventPage from "../../components/event/EventPage"
+
+class EventContainer extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EventContainer
