cf16e97a7b3ec9578c8930dd8bf064d75656f80a
jamescross813 <crossjames813@gmail.com>
2021-10-30 14:46:11 -0500

    initial state

--- src/containers/group/GroupsListContainer.js
+++ src/containers/group/GroupsListContainer.js
@@ -2,6 +2,11 @@ import { Component } from "react";
 import GroupsList from  "../../components/group/GroupsList"
 
 class GroupsListContainer extends Component{
+
+    state={
+        groups: []
+    }
+    
     render(){
         return(
         <div>
