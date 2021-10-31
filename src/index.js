79510f1fdc25dc838da3cab926ca7db7ce3caab6
jamescross813 <crossjames813@gmail.com>
2021-10-30 23:27:00 -0500

    added userReducer

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import thunk from "redux-thunk";
+import userReducer from './src/reducer/userReducer'
 
-const store= createStore(applyMiddleware(thunk))
+const store= createStore(userReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
