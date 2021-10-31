e6727adf427720b1fb457ce5639f8aeeffd4c637
jamescross813 <crossjames813@gmail.com>
2021-10-31 16:39:07 -0500

    added ink

--- src/components/event/EventsPage.js
+++ src/components/event/EventsPage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import EventInputContainer from "../../containers/event/EventInputContainer";
 import EventsListContainer from "../../containers/event/EventsListContainer";
+import { Link } from "react-router-dom";
 
 const EventsPage = ({handleClick}) =>{
     return(
     <div>
         <h3>Events Page</h3>
         <EventsListContainer />
-        <button onClick={handleClick}>Add</button>
+        <Link to="/event/add">
+        <button>Add</button>
+        </Link>
     </div>
     )
 }
