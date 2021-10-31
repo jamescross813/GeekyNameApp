0f9effdac8492ed89904b7c18dee3a148a087f8f
jamescross813 <crossjames813@gmail.com>
2021-10-31 14:41:51 -0500

    render eventslist

--- src/components/event/EventsPage.js
+++ src/components/event/EventsPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import EventsListContainer from "../../containers/event/EventsListContainer";
 
 const EventsPage = ()=>{
     return(
     <div>
         <h3>Events Page</h3>
+        <EventsListContainer />
     </div>
     )
 }
