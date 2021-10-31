4a1736e22818435ad05b8880029fd81769f266a7
jamescross813 <crossjames813@gmail.com>
2021-10-31 17:15:51 -0500

    send to user homepage added

--- src/containers/event/EventInputContainer.js
+++ src/containers/event/EventInputContainer.js
@@ -22,7 +22,8 @@ class EventInputContainer extends Component{
           };
           fetch("http://localhost:3000/events", configObj)
             .then((r) => r.json())
-            .then(data=>console.log(data))
+            // .then(data=>console.log(data))
+            .then(this.props.history.push("/user"))
             
     }
 
