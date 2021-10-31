3da848a85201bb2aa5d66243111c4a2b674e8c5f
jamescross813 <crossjames813@gmail.com>
2021-10-31 18:01:09 -0500

    createFriend function added

--- src/containers/friend/FriendInputContainer.js
+++ src/containers/friend/FriendInputContainer.js
@@ -4,6 +4,30 @@ import {connect} from "react-redux"
 
 class EventInputContainer extends Component{
 
+    createFriend=(formData)=>{
+        let friendInfo = {
+            friend: {
+                username: formData.username,
+                user_id: this.props.user.user.id
+            },
+          }
+          let configObj = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(friendInfo)
+          };
+          fetch("http://localhost:3000/friends", configObj)
+            // .then((r) => r.json())
+            .then((r)=>console.log(r))
+            // .then(data=>console.log(data))
+            // .then(this.props.history.push("/friends"))
+            
+    }
+
+
     render(){
         return(
             <div>
