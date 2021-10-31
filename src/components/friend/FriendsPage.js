57a09ec61f9cca5051f955d677ca462bdee66b63
jamescross813 <crossjames813@gmail.com>
2021-10-31 17:55:18 -0500

    link added

--- src/components/friend/FriendsPage.js
+++ src/components/friend/FriendsPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import FriendsListContainer from "../../containers/friend/FriendsListContainer";
+import { Link } from "react-router-dom";
 
 const FriendsPage = () =>{
     return(
     <div>
         <h3>Friends Page</h3>
         <FriendsListContainer />
+        <Link to="/friend/add">
+            <button>Add</button>
+        </Link>
     </div>
     )
 }
