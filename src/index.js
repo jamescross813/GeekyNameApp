c0418bf68cc299652d6793886003e44c8c2aed65
jamescross813 <crossjames813@gmail.com>
2021-10-30 23:21:52 -0500

    added thunk

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import thunk from "redux-thunk";
 
-const store= createStore()
+const store= createStore(applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
