b0ece779a088a940699b34e593bb4d8aa8a3229f
jamescross813 <crossjames813@gmail.com>
2021-10-31 16:49:54 -0500

    form built out

--- src/components/event/EventInput.js
+++ src/components/event/EventInput.js
@@ -19,13 +19,13 @@ class EventInput extends Component{
         return(
             <form>
                 <label>Event Title:</label>
-                <input type="text"/><br/>
+                <input type="text" name="event_name" value={this.state.event_name} onChange={this.handleChange}/><br/>
                 <label>Event Description:</label>
-                <input type="text"/><br/>
+                <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/><br/>
                 <label>Estimated Time:</label>
-                <input type="text"/><br/>
+                <input type="text" name="est_time" value={this.state.est_time} onChange={this.handleChange}/><br/>
                 <label>Game Type:</label>
-                <input type="text"/><br/>
+                <input type="text" name="game_type" value={this.state.game_type} onChange={this.handleChange}/><br/>
                 <input type="submit"/>
             </form>
         )
