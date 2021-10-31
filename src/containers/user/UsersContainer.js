c91b273fe538aa4d1f94c9898f84bb08a259376b
jamescross813 <crossjames813@gmail.com>
2021-10-31 09:28:47 -0500

    response passes to json()

--- src/containers/user/UsersContainer.js
+++ src/containers/user/UsersContainer.js
@@ -4,7 +4,8 @@ class UserContainer extends Component{
 
     fetchUsers=()=>{
         fetch("http://localhost:3000/users")
-        .then(r=>console.log(r))
+        .then(r=>r.json())
+        .then(data=>console.log(data))
 
     }
     render(){
