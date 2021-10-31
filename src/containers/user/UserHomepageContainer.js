89402baae5b85bec963a51468e02d445c463c083
jamescross813 <crossjames813@gmail.com>
2021-10-30 21:11:11 -0500

    moved fetch

--- src/containers/user/UserHomepageContainer.js
+++ src/containers/user/UserHomepageContainer.js
@@ -3,6 +3,29 @@ import UserHomepage from "../../components/user/UserHomepage"
 
 class UserHomepageContainer extends Component{
 
+    login = (formData) =>{
+        this.fetchSession(formData)
+      }
+    
+      fetchSession(formData){
+        console.log(formData)
+        fetch("http://localhost:3000/login",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            username: formData.username,
+            password: formData.password,
+          },
+        }),
+      })
+        .then((r) => r.json())
+        .then(data=> <Link to="/user/1" userData={data}/>);
+        }
+
     render(){
         // console.log(this.props)
         return(
