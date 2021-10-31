9f6d4041e38bf7aac1c3a77bf69af5d3c72ad86e
jamescross813 <crossjames813@gmail.com>
2021-10-31 14:39:05 -0500

    render friendslist

--- src/components/friend/FriendsPage.js
+++ src/components/friend/FriendsPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import FriendsListContainer from "../../containers/friend/FriendsListContainer";
 
 const FriendsPage = () =>{
     return(
     <div>
         <h3>Friends Page</h3>
+        <FriendsListContainer />
     </div>
     )
 }
