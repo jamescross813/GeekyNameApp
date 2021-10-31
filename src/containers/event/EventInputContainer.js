be809e6f0c3b3bcac413d869e7938642f462f816
jamescross813 <crossjames813@gmail.com>
2021-10-31 17:09:04 -0500

    attempt at fetch method

--- src/containers/event/EventInputContainer.js
+++ src/containers/event/EventInputContainer.js
@@ -4,7 +4,25 @@ import EventInput from "../../components/event/EventInput"
 class EventInputContainer extends Component{
 
     createEvent=(formData)=>{
-        console.log(formData)
+        let eventInfo = {
+            event: {
+                event_name: formData.event_name,
+                description: formData.description,
+                est_time: formData.est_time,
+                game_type: formData.game_type
+            },
+          }
+          let configObj = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(eventInfo)
+          };
+          fetch("http://localhost:3000/events", configObj)
+            .then((r) => console.log(r))
+            
     }
 
     render(){
