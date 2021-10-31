eee0ae4a5d776549d68581da464b5547a6edff32
jamescross813 <crossjames813@gmail.com>
2021-10-31 16:57:12 -0500

    added createEvent function

--- src/containers/event/EventInputContainer.js
+++ src/containers/event/EventInputContainer.js
@@ -3,6 +3,10 @@ import EventInput from "../../components/event/EventInput"
 
 class EventInputContainer extends Component{
 
+    createEvent=(formData)=>{
+        console.log(formData)
+    }
+
     render(){
         return(
             <div>
