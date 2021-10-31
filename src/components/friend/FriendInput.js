c4ae5f06fa496f556f77688a9b25c854173c2bf7
jamescross813 <crossjames813@gmail.com>
2021-10-31 17:50:06 -0500

    file added

---
+++ src/components/friend/FriendInput.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class FormInput extends Component{
+
+    state={
+        
+    }
+
+    render(){
+        return(
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default FormInput
