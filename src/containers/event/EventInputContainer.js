a253c80eb5807f1116258f40285e0b684743957a
jamescross813 <crossjames813@gmail.com>
2021-10-31 15:57:38 -0500

    basic structure

---
+++ src/containers/event/EventInputContainer.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import EventInput from "../../components/event/EventInput"
+
+class EventInputContainer extends Component{
+
+    render(){
+        return(
+            <div>
+                <EventInput />
+            </div>
+        )
+    }
+}
+
+export default EventInputContainer
