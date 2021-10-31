7cb5fc4eebcb89b7f80d8a1e17bcb6858038a161
jamescross813 <crossjames813@gmail.com>
2021-10-31 18:44:05 -0500

    basic fetch included

--- src/containers/group/GroupInputContainer.js
+++ src/containers/group/GroupInputContainer.js
@@ -5,7 +5,23 @@ import {connect} from "react-redux"
 class GroupInputContainer extends Component{
 
     createGroup=(formData)=>{
-        console.log(formData)
+        let groupInfo = {
+            group: {
+                group_name: formData.group_name,
+                user_id: this.props.user.user.id
+            },
+          }
+          let configObj = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(groupInfo)
+          };
+          fetch("http://localhost:3000/groups", configObj)
+            .then((r) => r.json())
+            // .then((r)=>console.log(r))
     }
 
     render(){
