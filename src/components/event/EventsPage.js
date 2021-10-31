8c09f26584363fcb1fa27571c41de7a8e5526398
jamescross813 <crossjames813@gmail.com>
2021-10-31 15:24:37 -0500

    following bug issues

--- src/components/event/EventsPage.js
+++ src/components/event/EventsPage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import EventsListContainer from "../../containers/event/EventsListContainer";
+// import EventsListContainer from "../../containers/event/EventsListContainer";
 
 const EventsPage = ()=>{
     return(
     <div>
         <h3>Events Page</h3>
-        <EventsListContainer />
+        {/* <EventsListContainer /> */}
     </div>
     )
 }
