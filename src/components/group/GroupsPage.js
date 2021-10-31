d706bc98f60d3d379837e22bd31ce001d7687122
jamescross813 <crossjames813@gmail.com>
2021-10-31 14:40:04 -0500

    render groupslist

--- src/components/group/GroupsPage.js
+++ src/components/group/GroupsPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GroupsListContainer from "../../containers/group/GroupsListContainer";
 
 const GroupsPage = () =>{
     return(
         <div>
             <h3>Groups Page</h3>
+            <GroupsListContainer />
         </div>
     )
 }
