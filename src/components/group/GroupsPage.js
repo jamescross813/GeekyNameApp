7abf7ce5f1b9c7f670448c77d1ead613474fc3e8
jamescross813 <crossjames813@gmail.com>
2021-10-31 18:20:26 -0500

    link added

--- src/components/group/GroupsPage.js
+++ src/components/group/GroupsPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import GroupsListContainer from "../../containers/group/GroupsListContainer";
+import { Link } from "react-router-dom";
 
 const GroupsPage = () =>{
     return(
         <div>
             <h3>Groups Page</h3>
             <GroupsListContainer />
+            <Link to="/group/add">
+                <button>Add</button>
+            </Link>
         </div>
     )
 }
