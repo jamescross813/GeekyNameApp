877d0c9ac677a56fdf7af37a69e4d9c19eadf3fd
jamescross813 <crossjames813@gmail.com>
2021-10-31 00:43:49 -0500

    added in state based changes

--- src/containers/user/LoginFormContainer.js
+++ src/containers/user/LoginFormContainer.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom"
-import UserHomepageContainer from "../../containers/user/UserHomepageContainer";
+import UserHomepageContainer from "./UserHomepageContainer";
 import { connect } from 'react-redux';
 
 
 class LoginForm extends Component {
 
-    state = {
-        username: "",
-        password: ""
-    }
-
-    handleChange = (event) =>{
-        this.setState({
-          [event.target.name]: event.target.value
-        })
-      }
-
-    handleSubmit = (event) =>{
-      console.log(this.props)
-        event.preventDefault()
+    login = (userData) =>{
         let loginInfo = {
                   user: {
-                    username: this.state.username,
-                    password: this.state.password,
+                    username: userData.username,
+                    password: userData.password,
                   },
                 }
                 let configObj = {
@@ -43,13 +30,9 @@ class LoginForm extends Component {
 
     render() {  
         return (
-          <form onSubmit={this.handleSubmit}>
-              <label>Username</label><br/>
-              <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br/>
-              <label>Password</label><br/>
-              <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br/>
-                <input type="submit" />
-          </form>
+         <div>
+           <LoginForm handleLogin={this.login} />
+         </div>
         );
       }
 };
