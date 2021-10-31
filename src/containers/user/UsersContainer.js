90bee9ae351a04d1ed9b4b6c83e5f21f8ded9447
jamescross813 <crossjames813@gmail.com>
2021-10-31 09:18:24 -0500

    start of fetch function

--- src/containers/user/UsersContainer.js
+++ src/containers/user/UsersContainer.js
@@ -1,9 +1,15 @@
 import { Component } from "react";
 
 class UserContainer extends Component{
+
+    fetchUsers=()=>{
+        fetch("http://localhost:3000/users")
+
+    }
     render(){
         <div></div>
     }
 }
 
 export default UserContainer
+
