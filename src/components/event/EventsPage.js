d0ea28c03a37771adb64e324922a422358e3da63
jamescross813 <crossjames813@gmail.com>
2021-10-31 16:11:11 -0500

    eventlistener working

--- src/components/event/EventsPage.js
+++ src/components/event/EventsPage.js
@@ -2,12 +2,12 @@ import React from "react";
 import EventInputContainer from "../../containers/event/EventInputContainer";
 import EventsListContainer from "../../containers/event/EventsListContainer";
 
-const EventsPage = ()=>{
+const EventsPage = ({handleClick}) =>{
     return(
     <div>
         <h3>Events Page</h3>
         <EventsListContainer />
-        <button>Add</button>
+        <button onClick={handleClick}>Add</button>
     </div>
     )
 }
