425c8b4e87da89cc1c7d769623229b10e98797ea
jamescross813 <crossjames813@gmail.com>
2021-10-31 18:18:38 -0500

    files added along with basic structure

---
+++ src/containers/group/FriendInputContainer.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import GroupInput from "../../components/group/GroupInput"
+import {connect} from "react-redux"
+
+class GroupInputContainer extends Component{
+
+    render(){
+        return(
+            <div>
+                <GroupInput />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps=(state)=>{
+    return{user: state.user}
+}
+
+export default connect()(GroupInputContainer)
