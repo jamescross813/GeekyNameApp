0c96cf9c8a05240f4bf3b79706d6a91507197fe6
jamescross813 <crossjames813@gmail.com>
2021-10-31 19:14:01 -0500

    fixed typo

--- src/reducers/friendsReducer.js
+++ src/reducers/friendsReducer.js
@@ -2,10 +2,11 @@ function friendsReducer(state={
     friends:{},
 },
 action){
+
     switch(action.type){
         case "ADD_FRIEND":
             return{
-                friends: action.userData
+                friends: action.friendData
             }
             default:
                 return state
