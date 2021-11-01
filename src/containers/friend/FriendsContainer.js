0d305b1f5798612e1bdf70094d0ee5a1858eefa2
jamescross813 <crossjames813@gmail.com>
2021-10-31 19:02:24 -0500

    basic class written

---
+++ src/containers/friend/FriendsContainer.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { connect } from "react-redux"
+
+class FriendsContainer extends Component{
+
+    render(){
+        return(
+        <div>
+            
+        </div>
+        )
+    }
+}
+
+export default connect()(FriendsContainer)
+
