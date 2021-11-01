1fbcf6007b01f11a2ad878af690e99f2768d004e
jamescross813 <crossjames813@gmail.com>
2021-10-31 21:26:59 -0500

    mapDispatchToProps

--- src/containers/event/EventsListContainer.js
+++ src/containers/event/EventsListContainer.js
@@ -41,8 +41,8 @@ class EventsListContainer extends Component{
         return(
             this.state.events.map((event)=>{
                 if(event.id === id){
-                    return <EventsList eventInfo={event} />
-                    // console.log(event)
+                    // return <EventsList eventInfo={event} />
+                    console.log(event)
                 }
             })
         )
@@ -66,4 +66,7 @@ const mapStateToProps=(state)=>{
     }
 }
 
-export default connect(mapStateToProps)(EventsListContainer)
+const mapDispatchToProps=(dispatch)=>{}
+    
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsListContainer)
