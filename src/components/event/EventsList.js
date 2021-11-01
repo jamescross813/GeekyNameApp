0d10e1506077280f75e7752f61798cbcb685ce55
jamescross813 <crossjames813@gmail.com>
2021-10-31 19:06:10 -0500

    console.log commented out

--- src/components/event/EventsList.js
+++ src/components/event/EventsList.js
@@ -6,7 +6,7 @@ const EventsList = ({eventInfo})=>{
         <ul>
             <li>
                 {eventInfo.event_name}
-                {console.log(eventInfo.event_name)}
+                {/* {console.log(eventInfo.event_name)} */}
                 {/* You are here */}
             </li>
         </ul>
