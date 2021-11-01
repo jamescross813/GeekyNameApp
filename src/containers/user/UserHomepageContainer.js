bdc2d9166a4af80ec1fc3240c581549c706bf3b3
jamescross813 <crossjames813@gmail.com>
2021-10-31 19:25:18 -0500

    commented out friendscontainer

--- src/containers/user/UserHomepageContainer.js
+++ src/containers/user/UserHomepageContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import UserHomepage from "../../components/user/UserHomepage"
 import {Route} from "react-router-dom"
 import {connect} from "react-redux"
-import FriendsContainer from "../friend/FriendsContainer";
+// import FriendsContainer from "../friend/FriendsContainer";
 
 class UserHomepageContainer extends Component{
     
@@ -11,7 +11,7 @@ class UserHomepageContainer extends Component{
         <div>
           <UserHomepage userInfo={this.props.user}/>
           {/* {console.log(this.props.user)} */}
-          <FriendsContainer />
+          {/* <FriendsContainer /> */}
         </div>
         )
     }
