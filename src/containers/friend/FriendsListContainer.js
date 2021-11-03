9543971b76f9fcff721a24b8a4e2cf7b732c7aa5
jamescross813 <crossjames813@gmail.com>
2021-11-02 20:20:07 -0500

    updated to add to state

--- src/containers/friend/FriendsListContainer.js
+++ src/containers/friend/FriendsListContainer.js
@@ -4,27 +4,20 @@ import {connect} from "react-redux"
 
 class FriendsListContainer extends Component{
 
-    state = { 
-        friends: []
-    }
-
     componentDidMount(){
         fetch("http://localhost:3000/friends")
         .then(resp=>resp.json())
-        .then(data=>{
-            this.setState({
-                friends: data
-            })
-        })
+        .then(data=>this.props.setFriendsInfo(data))
+        // .then(data=>console.log(data))
     }
 
         gatherList=()=>(
-            this.state.friends.map((friend)=>{
+            this.props.friendsInfo.friends.map((friend)=>{
                 if(friend.user_id === this.props.userInfo.user.id) {
                     return this.gatherListInfo(friend.friend_user_id)
-                }            
-    
+                }           
             })
+           
         )
 
         gatherListInfo = (id)=>{
@@ -49,8 +42,18 @@ class FriendsListContainer extends Component{
     const mapStateToProps=(state)=>{
         return{
             userInfo: state.userInfo,
-            usersInfo: state.usersInfo
+            usersInfo: state.usersInfo,
+            friendsInfo: state.friendsInfo
         }
     }
+
+    const mapDispatchToProps=(dispatch)=>{
+        return {
+            setFriendsInfo: (friends) => dispatch({
+              type: "ADD_FRIEND",
+              friendData: friends
+            })
+          };
+    }
     
-    export default connect(mapStateToProps)(FriendsListContainer)
+    export default connect(mapStateToProps, mapDispatchToProps)(FriendsListContainer)
