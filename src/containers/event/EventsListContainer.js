024abffd0d4c995bf5f353acfc1bbb8319187475
jamescross813 <crossjames813@gmail.com>
2021-11-02 21:44:17 -0500

    removed unused code

--- src/containers/event/EventsListContainer.js
+++ src/containers/event/EventsListContainer.js
@@ -66,7 +66,7 @@ const mapStateToProps=(state)=>{
     }
 }
 
-const mapDispatchToProps=(dispatch)=>{}
+
     
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventsListContainer)
+export default connect(mapStateToProps)(EventsListContainer)
