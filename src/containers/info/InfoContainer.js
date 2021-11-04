4ae29142f276abf5fee31b4d63b614478141f713
jamescross813 <crossjames813@gmail.com>
2021-11-03 22:42:18 -0500

    basic structure

---
+++ src/containers/info/InfoContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import FriendsContainer from "../friend/FriendsContainer";
+
+class InfoContainer extends Component{
+
+    redner(){
+        <div>
+            <FriendsContainer />
+        </div>
+    }
+
+}
+
+export default InfoContainer
