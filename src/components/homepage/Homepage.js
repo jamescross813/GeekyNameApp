cf79ca439b79e5cec7949d90bdae21d294891abf
jamescross813 <crossjames813@gmail.com>
2021-11-03 22:32:45 -0500

    formatting

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -12,7 +12,7 @@ class Homepage extends Component{
             <h1>Geeky Name App</h1>
             <h4>About us</h4>
             <p>This app is a way for geeks and nerds to share events and availabilty to ensure games happen and geeks stay united.</p>
-
+            
         </div>
         )
     }
