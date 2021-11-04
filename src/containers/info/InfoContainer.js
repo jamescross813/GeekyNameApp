abc9e60ba3d727f24db8d3b425a1c4f3f25dbd57
jamescross813 <crossjames813@gmail.com>
2021-11-03 22:42:54 -0500

    Users container included:

--- src/containers/info/InfoContainer.js
+++ src/containers/info/InfoContainer.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import FriendsContainer from "../friend/FriendsContainer";
+import UsersContainer from "../user/UsersContainer";
 
 class InfoContainer extends Component{
 
     redner(){
         <div>
             <FriendsContainer />
+            <UsersContainer />
         </div>
     }
 
