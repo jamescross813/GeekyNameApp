a359aedd83707d40cb2248285796323504e091d1
jamescross813 <crossjames813@gmail.com>
2021-11-04 22:53:59 -0500

    events reducer added to root

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import usersReducer from "./usersReducer";
 import friendsReducer from "./friendsReducer";
-import groupsReducer from "./groupsReducer"
+import groupsReducer from "./groupsReducer";
+import eventsReducer from "./eventReducer";
 
 const rootReducer = combineReducers({
   userInfo: userReducer,
   usersInfo: usersReducer,
   friendsInfo: friendsReducer,
-  groupsInfo: groupsReducer
+  groupsInfo: groupsReducer,
+  eventsInfo: eventsReducer
   });
   
   export default rootReducer;
