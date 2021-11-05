cfd12de2403de240478810be3ffc1b54f35d817f
jamescross813 <crossjames813@gmail.com>
2021-11-04 22:57:21 -0500

    basic structure added

---
+++ src/containers/event/EventsContainer.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import { connect } from "react-redux"
+
+class EventsContainer extends Component{
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsContainer)
+
