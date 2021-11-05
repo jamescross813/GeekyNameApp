04e0212c05c85fd291709580b97e3eb8737e4b1c
jamescross813 <crossjames813@gmail.com>
2021-11-04 22:58:38 -0500

    logic added

--- src/containers/event/EventsContainer.js
+++ src/containers/event/EventsContainer.js
@@ -7,11 +7,45 @@ class EventsContainer extends Component{
         events: []
     }
     
+    componentDidMount(){
+        fetch("http://localhost:3000/events")
+        .then(resp=>resp.json())
+        .then(data=>this.setState({
+            events: data
+            })
+        )
+        .then(data=>this.gatherInfo(data))
+    }
+            
+    gatherInfo=(data)=>{
+        this.state.events.map((event)=>{
+            return this.gatherListInfo(event.user_events)
+            }
+        )     
+    }
+
+    gatherListInfo=(userevent)=>{
+        userevent.map((event)=>{
+            if(event.user_id === this.props.userInfo.user.id){
+                 return this.finalInfo(event.id)
+            }
+        }) 
+    }  
+
+    finalInfo=(id)=>{
+        return this.state.events.map((event)=>{
+                if(event.id === id){
+                    return this.props.seteventsInfo(event)
+                }
+            }
+        )
+    }
     
 
     render(){
         return(
             <div>
+                {console.log(this.props)}
             </div>
         )
     }
@@ -29,7 +63,7 @@ const mapStateToProps=(state)=>{
 const mapDispatchToProps=(dispatch)=>{
     return {
         seteventsInfo: (event) => dispatch({
-          type: "ADD_event",
+          type: "ADD_EVENT",
           eventData: event
         })
       };
