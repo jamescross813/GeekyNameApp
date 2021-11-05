78c098c9c3a3799ef6a7d50017c9f98421da0e64
jamescross813 <crossjames813@gmail.com>
2021-11-04 21:49:10 -0500

    removed unused code

--- src/containers/group/GroupsListContainer.js
+++ src/containers/group/GroupsListContainer.js
@@ -4,47 +4,16 @@ import {connect} from "react-redux"
 
 class GroupsListContainer extends Component{
 
-    state={
-        groups: []
+    gatherList=()=>{
+        console.log(this.props.groupsInfo)
+        // return(
+        //     this.props.groupsInfo.groups.map((group)=>{
+        //         return <GroupsList GroupInfo={group} />
+        //     })
+        // )
     }
-
-    componentDidMount(){
-        fetch("http://localhost:3000/groups")
-        .then(resp=>resp.json())
-        .then(data=>{
-            this.setState({
-                groups: data
-            })
-        })
-    }
-
-    gatherList=()=>(
-        this.state.groups.map((group)=>{
-            return(
-                this.gatherListInfo(group.user_groups)
-                // console.log(group.user_groups)
-                )
-            }             
-        )
-    )
-
-    gatherListInfo=(userGroup)=>(
-        userGroup.map((group)=>{
-            if(group.user_id === this.props.userInfo.user.id){
-                return this.finalInfo(group.id)
-            }
-        })
-    )
-
-    finalInfo=(id)=>{
-        return(
-            this.state.groups.map((group)=>{
-                if(group.id === id){
-                    return <GroupsList groupInfo={group} />
-                }
-            })
-        )
-    }
+        
+    
 
     render(){
         return(
@@ -57,7 +26,8 @@ class GroupsListContainer extends Component{
 
 const mapStateToProps=(state)=>{
     return{
-        userInfo: state.userInfo
+        userInfo: state.userInfo,
+        groupsInfo: state.groupsInfo
     }
 }
 
