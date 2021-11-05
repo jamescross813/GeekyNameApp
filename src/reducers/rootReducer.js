b4ddef193f73601c3ee4c92d4060fd52b24d1f64
jamescross813 <crossjames813@gmail.com>
2021-11-04 22:20:46 -0500

    added group reducer to root

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import usersReducer from "./usersReducer";
 import friendsReducer from "./friendsReducer";
+import groupsReducer from "./groupsReducer"
 
 const rootReducer = combineReducers({
   userInfo: userReducer,
   usersInfo: usersReducer,
-  friendsInfo: friendsReducer
+  friendsInfo: friendsReducer,
+  groupsInfo: groupsReducer
   });
   
   export default rootReducer;
