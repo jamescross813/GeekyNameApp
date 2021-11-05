707ec3ab39b88a38d4b7cccaec6e3019173dbb7c
jamescross813 <crossjames813@gmail.com>
2021-11-04 22:58:03 -0500

    maptoprops added

--- src/containers/event/EventsContainer.js
+++ src/containers/event/EventsContainer.js
@@ -3,7 +3,38 @@ import { connect } from "react-redux"
 
 class EventsContainer extends Component{
 
+    state = {
+        events: []
+    }
+    
+    
+
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps=(state)=>{
+    return{
+        userInfo: state.userInfo,
+        usersInfo: state.usersInfo,
+        friendsInfo: state.friendsInfo,
+        eventsInfo: state.eventsInfo
+    }
+}
+
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        seteventsInfo: (event) => dispatch({
+          type: "ADD_event",
+          eventData: event
+        })
+      };
+}
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsContainer)
 
