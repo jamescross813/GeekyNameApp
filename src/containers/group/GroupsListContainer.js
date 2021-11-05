29cb60554cf731cd359692f5a2fcb618fe858e87
jamescross813 <crossjames813@gmail.com>
2021-11-04 23:02:01 -0500

    removed console.log

--- src/containers/group/GroupsListContainer.js
+++ src/containers/group/GroupsListContainer.js
@@ -16,7 +16,6 @@ class GroupsListContainer extends Component{
         return(
         <div>
             {this.gatherList()}
-            {console.log(this.props.groupsInfo.groups)}
         </div>
         )
     }
