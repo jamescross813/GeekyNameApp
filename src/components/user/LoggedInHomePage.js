6c0f24ccbc46065c377871ec4ef63067adb9f789
jamescross813 <crossjames813@gmail.com>
2021-11-06 17:24:29 -0500

    basic structure

---
+++ src/components/user/LoggedInHomePage.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class LoggedInHomepage extends Component{
+    render(){
+        return(
+            <div>
+                <button>
+                    Let's Go!
+                </button>
+            </div>
+        )
+    }
+
+}
+
+export default LoggedInHomepage
