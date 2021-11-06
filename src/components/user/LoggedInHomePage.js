53b3c750baeb7ae44486ee39da102f193b312469
jamescross813 <crossjames813@gmail.com>
2021-11-06 17:26:20 -0500

    link info added

--- src/components/user/LoggedInHomePage.js
+++ src/components/user/LoggedInHomePage.js
@@ -1,12 +1,15 @@
 import { Component } from "react";
+import {Link} from "react-router-dom"
 
 class LoggedInHomepage extends Component{
     render(){
         return(
             <div>
+                <Link to="/user">
                 <button>
                     Let's Go!
                 </button>
+                </Link>
             </div>
         )
     }
