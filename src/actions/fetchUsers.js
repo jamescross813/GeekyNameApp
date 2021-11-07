1a7b16183bb829a57c42249cf74b3a936df93cb5
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:08:03 -0500

    attempt to use thunk

--- src/actions/fetchUsers.js
+++ src/actions/fetchUsers.js
@@ -1,5 +1,8 @@
 export function fetchUsers(){
     return(dispatch)=>{
-        
+        dispatch({type: "START_ADDING_USERS_REQUEST"});
+        fetch("http://localhost:3000/users")
+        .then(r=>r.json())
+        .then((data) => dispatch({type: "ADD_USERS", data}))
     }
 }
