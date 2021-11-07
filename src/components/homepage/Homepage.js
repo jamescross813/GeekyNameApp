ff1b307019688b44db0f94a93b7eaf21489ea76b
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:37:36 -0500

    tidy up

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,22 +1,18 @@
 import { Component } from "react";
-// import { Link } from "react-router-dom"
-// import InfoContainer from "../../containers/info/InfoContainer";
 import HomepageNavBar from "../navbar/HomepageNavBar";
-// import LoginForm from "../user/LoginForm"
 
 class Homepage extends Component{
 
     render(){
         return(
-        <div>
-            <HomepageNavBar />
-            <h1>Geeky Name App</h1>
-            <h4>About us</h4>
-            <p>This app is a way for geeks and nerds to share events and availabilty to ensure games happen and geeks stay united.</p>
-        </div>
+            <div>
+                <HomepageNavBar />
+                    <h1>Geeky Name App</h1>
+                        <h4>About us</h4>
+                             <p>This app is a way for geeks and nerds to share events and availabilty to ensure games happen and geeks stay united.</p>
+            </div>
         )
     }
-
 }
 
 export default Homepage
