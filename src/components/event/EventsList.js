f44ed33921d9909cc713b408da129749d992cdb2
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:32:39 -0500

    tidy up

--- src/components/event/EventsList.js
+++ src/components/event/EventsList.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const EventsList = ({eventInfo})=>{
     return(
-    <div>
-        <ul>
-            <li>
-                {eventInfo.event_name}
-            </li>
-        </ul>
-    </div>
+        <div>
+            <ul>
+                <li>
+                    {eventInfo.event_name}
+                </li>
+            </ul>
+        </div>
     )
 }
 
