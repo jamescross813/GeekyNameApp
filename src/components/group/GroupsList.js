1d7c836c196f07498280c8930f70f5b94ef482f4
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:36:37 -0500

    tidy up

--- src/components/group/GroupsList.js
+++ src/components/group/GroupsList.js
@@ -1,26 +1,18 @@
-import {Component} from "react";
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom"
 
-class GroupsList extends Component{
-
-    // onClick=(event)=>{
-    //     event.preventDefault()
-    //     return <GroupPageContainer groupInfo={this.props.groupInfo} />
-    // }
-
-    render(){
-        return(
+const GroupsList=()=>{
+    return(
         <div>
             <ul>
                 <li>
-                <Link key={this.props.group_id} to={`/groups/${this.props.groupInfo.id}`}>
-                    {this.props.groupInfo.group_name}
-                </Link>
+                    <Link key={this.props.group_id} to={`/groups/${this.props.groupInfo.id}`}>
+                        {this.props.groupInfo.group_name}
+                    </Link>
                 </li>
             </ul>
         </div>
-        )
-    }
+    )
 }
 
 export default GroupsList
