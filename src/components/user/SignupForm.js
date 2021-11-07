5f07d855fa530f83167567b140f4aa82e6db56d4
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:39:56 -0500

    tidy up

--- src/components/user/SignupForm.js
+++ src/components/user/SignupForm.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom"
-import UserHomepageContainer from "../../containers/user/UserHomepageContainer";
-import { connect } from 'react-redux';
-
 
 class SignupForm extends Component {
 
@@ -18,7 +14,6 @@ class SignupForm extends Component {
       }
 
     handleSubmit = (event) =>{
-      // console.log(this.props)
       event.preventDefault()
       this.props.handleLogin(this.state)
     }
@@ -27,9 +22,9 @@ class SignupForm extends Component {
         return (
           <form onSubmit={this.handleSubmit}>
               <label>Username</label><br/>
-              <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br/>
+                <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br/>
               <label>Password</label><br/>
-              <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br/>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br/>
                 <input type="submit" />
           </form>
         );
