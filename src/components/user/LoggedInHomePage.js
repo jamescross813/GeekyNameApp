dfd205cabbc72d44e95dacd2b21c55e2e266a291
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:39:04 -0500

    tidy up

--- src/components/user/LoggedInHomePage.js
+++ src/components/user/LoggedInHomePage.js
@@ -1,19 +1,17 @@
-import { Component } from "react";
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom"
 
-class LoggedInHomepage extends Component{
-    render(){
-        return(
-            <div>
-                <Link to="/user">
+const LoggedInHomepage=()=>{
+    return(
+        <div>
+            <Link to="/user">
                 <button>
                     Let's Go!
                 </button>
-                </Link>
-            </div>
-        )
-    }
-
+            </Link>
+        </div>
+    )
 }
+
 
 export default LoggedInHomepage
