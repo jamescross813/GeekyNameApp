548e671c07fd8bb536f68b4306a3db9111d59f4b
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:43:25 -0500

    tidy up

--- src/containers/user/UserHomepageContainer.js
+++ src/containers/user/UserHomepageContainer.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import UserHomepage from "../../components/user/UserHomepage"
-// import {Route} from "react-router-dom"
 import {connect} from "react-redux"
-// import FriendsContainer from "../friend/FriendsContainer";
 
 class UserHomepageContainer extends Component{
 
