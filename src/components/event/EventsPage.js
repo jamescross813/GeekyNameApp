f0ae655411486ecd173decd01663cbf1d10a683b
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:31:04 -0500

    tidy up

--- src/components/event/EventsPage.js
+++ src/components/event/EventsPage.js
@@ -1,17 +1,16 @@
 import React from "react";
-// import EventInputContainer from "../../containers/event/EventInputContainer";
 import EventsListContainer from "../../containers/event/EventsListContainer";
 import { Link } from "react-router-dom";
 
-const EventsPage = ({handleClick}) =>{
+const EventsPage = () =>{
     return(
-    <div>
-        <h3>Events Page</h3>
-        <EventsListContainer />
-        <Link to="/event/add">
-        <button>Add</button>
-        </Link>
-    </div>
+        <div>
+            <h3>Events Page</h3>
+                <EventsListContainer />
+                <Link to="/event/add">
+                    <button>Add</button>
+                </Link>
+        </div>
     )
 }
 
