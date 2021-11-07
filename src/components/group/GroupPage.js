29bd72a37738d3db75075ac12b198b0c09c95da6
jamescross813 <crossjames813@gmail.com>
2021-11-06 21:32:32 -0500

    basic logic for finding friend ids

--- src/components/group/GroupPage.js
+++ src/components/group/GroupPage.js
@@ -1,12 +1,18 @@
 import {Component} from "react";
 
 class GroupPage extends Component {
+
+    friendInfo=()=>{
+        return this.props.groupInfo.friend_groups.map((friend)=>{
+            console.log(friend.friend_id)
+        })
+    }
     
     render(){
         return(
             <div>
                 <h2>{this.props.groupInfo.group_name}</h2>
-                {/* <li>{groupInfo.friend</li> */}
+                {this.friendInfo()}
             </div>
         )
     }
