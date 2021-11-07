4f3c2d9922000e619e8bd5f96c722c730e2fbf4d
jamescross813 <crossjames813@gmail.com>
2021-11-06 23:48:54 -0500

    tracking down bug

--- src/components/group/GroupPage.js
+++ src/components/group/GroupPage.js
@@ -2,13 +2,13 @@ import { Component } from "react";
 
 class GroupPage extends Component {
 
-    friendInfo=()=>{
+    friendInfo(){
         return this.props.groupInfo.friend_groups.map((friend)=>{
             return this.renderFriends(friend.friend_id)
         })
     }
 
-    renderFriends=(friendId)=>{
+    renderFriends(friendId){
         return this.props.friendsInfo.friends.map((friend)=>{
             if(friend.id === friendId ){
                 return <li>{friend.username}</li>
@@ -16,31 +16,31 @@ class GroupPage extends Component {
         })
     }
 
-    eventInfo=()=>{
+    eventInfo(){
         return this.props.eventsInfo.events.map((event)=>{
             event.group_events.map((groupEvent)=>{
-                if(groupEvent.group_id == this.props.groupInfo.id){
+               if(groupEvent.group_id === this.props.groupInfo.id){
                    return this.renderEvents(groupEvent.event_id)
                 }
             })
         })
     }
 
-    renderEvents=(eventId)=>(
-        this.props.eventsInfo.events.map((event)=>{
-            if(event.id === eventId){
-                // return <li>{event.event_name}</li>
+    renderEvents(eventId){
+        return this.props.eventsInfo.events.map((event)=>{
+            if(event.id === eventId ){
+                // return <p>{event.event_name}</p>
                 console.log(event.event_name)
             }
         })
-    )
+    }
     
     render(){
         return(
             <div>
                 <h2>{this.props.groupInfo.group_name}</h2>
                     <h4>Friends</h4>
-                        {/* {this.friendInfo()} */}
+                        {this.friendInfo()}
                     <h4>Events</h4>
                         {this.eventInfo()}
 
