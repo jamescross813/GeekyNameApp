a36fc04a45e62c1917d7f2d2839881b1fd18599b
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:28:57 -0500

    tidy up

--- src/components/user/UserHomepage.js
+++ src/components/user/UserHomepage.js
@@ -4,22 +4,22 @@ import {Link} from "react-router-dom"
 class UserHomepage extends Component{
     render(){
         return(
-        <div>
-            <h2>Welcome {this.props.userInfo.username}!</h2>
+            <div>
+                <h2>Welcome {this.props.userInfo.username}!</h2>
+                    
+                        <Link to= "/events">
+                            <h3>My Events</h3>
+                        </Link>
                 
-                    <Link to= "/events">
-                        <h3>My Events</h3>
-                    </Link>
-               
-                    <Link to= "/friends">
-                        <h3>My Friends
-                    </h3></Link>
-                
-                    <Link to= "/groups">
-                        <h3>My Groups</h3>
-                    </Link>
-                
-        </div>
+                        <Link to= "/friends">
+                            <h3>My Friends
+                        </h3></Link>
+                    
+                        <Link to= "/groups">
+                            <h3>My Groups</h3>
+                        </Link>
+                    
+            </div>
         )
     }
 }
