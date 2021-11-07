492cb9600cde05c43544bc93b8469c1784761404
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:34:27 -0500

    tidy up

--- src/components/group/GroupInput.js
+++ src/components/group/GroupInput.js
@@ -9,27 +9,28 @@ class GroupInput extends Component{
 
     handleChange = (event) =>{
         this.setState({
-          [event.target.name]: event.target.value
+            [event.target.name]: event.target.value
         })
       }
 
-      handleCheck=(event)=>{
-          this.setState({
-              friends: [...this.state.friends, event.target.name]
-          })
-      }
+    handleCheck=(event)=>{
+        this.setState({
+            friends: [...this.state.friends, event.target.name]
+        })
+    }
 
-      handleSubmit = (event)=>{
+    handleSubmit = (event)=>{
         event.preventDefault()
         this.props.handleCreate(this.state)
     }
 
     renderFriends=()=>{
        return this.props.friendsInfo.map((friend)=>{
-           return(<label>
-            <input type="checkbox" name={friend.username} key={friend.username} onChange={this.handleCheck}/>
-                {friend.username} <br></br>
-            </label>
+           return(
+           <label>
+                <input type="checkbox" name={friend.username} key={friend.username} onChange={this.handleCheck}/>
+                    {friend.username} <br></br>
+           </label>
             )
         })
     }
@@ -38,9 +39,9 @@ class GroupInput extends Component{
         return(
             <form onSubmit={this.handleSubmit}>
                 <label>Group Name:</label>
-                <input type="text" name="group_name" value={this.state.group_name} onChange={this.handleChange} /><br/>
-                {this.renderFriends()}
-                <input type="submit"/>
+                    <input type="text" name="group_name" value={this.state.group_name} onChange={this.handleChange} /><br/>
+                        {this.renderFriends()}
+                    <input type="submit"/>
             </form>
         )
     }
