cc3284425f81827633d1f4cce6f0ba4a3e028f6c
jamescross813 <crossjames813@gmail.com>
2021-11-06 21:41:16 -0500

    rendering friends in group

--- src/components/group/GroupPage.js
+++ src/components/group/GroupPage.js
@@ -4,15 +4,30 @@ class GroupPage extends Component {
 
     friendInfo=()=>{
         return this.props.groupInfo.friend_groups.map((friend)=>{
-            console.log(friend.friend_id)
+            return this.renderFriends(friend.friend_id)
         })
     }
+
+    renderFriends=(friendId)=>{
+        return this.props.friendsInfo.friends.map((friend)=>{
+            if(friend.id === friendId ){
+                return <li>{friend.username}</li>
+            }
+        })
+    }
+
+    // eventInfo=()=>{
+
+    // }
     
     render(){
         return(
             <div>
                 <h2>{this.props.groupInfo.group_name}</h2>
+                <h4>Friends</h4>
                 {this.friendInfo()}
+                <h4>Events</h4>
+                {/* {console.log(this.props.friendsInfo.friends)} */}
             </div>
         )
     }
