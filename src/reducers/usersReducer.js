8d3cf4246d76cd1b0770558c6ec3f5336ada14d5
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:17:52 -0500

    reducer updated

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,11 +1,18 @@
 function usersReducer(state={
     users:{},
+    requesting: false
 },
 action){
     switch(action.type){
+        case "START_ADDING_USERS_REQUEST":
+            return{
+                users: action.data,
+                requesting: true,
+            }
         case "ADD_USERS":
             return{
-                users: action.data
+                users: action.data,
+                requesting: false,
             }
             default:
                 return state
