87a31f248370a0b18986e94682185553dad5bf5e
jamescross813 <crossjames813@gmail.com>
2021-11-06 22:52:29 -0500

    tidy up

--- src/components/group/GroupsList.js
+++ src/components/group/GroupsList.js
@@ -1,18 +1,21 @@
-import React from "react";
+import {Component} from "react";
 import { Link } from "react-router-dom"
 
-const GroupsList=()=>{
-    return(
-        <div>
-            <ul>
-                <li>
-                    <Link key={this.props.group_id} to={`/groups/${this.props.groupInfo.id}`}>
-                        {this.props.groupInfo.group_name}
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    )
+class GroupsList extends Component{
+
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>
+                        <Link key={this.props.group_id} to={`/groups/${this.props.groupInfo.id}`}>
+                            {this.props.groupInfo.group_name}
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default GroupsList
